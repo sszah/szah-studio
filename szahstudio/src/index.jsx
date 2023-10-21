3be0852f42b55abf3c28c1ea75e9c98bf1620cc7
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-10-21 11:53:14 +0700

    components

---
+++ szahstudio/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './pages/App.jsx';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
