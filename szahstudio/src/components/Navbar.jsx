065253b12a1919a3d73f03b6a2bbd6e5373ffe73
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-10-21 19:51:35 +0700

    mini project

--- szahstudio/src/components/Navbar.jsx
+++ szahstudio/src/components/Navbar.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar
-        <div>Navbar</div>
-        <div>Navbar</div>
-    </div>
+    <div>Navbar</div>
   )
 }
--- szahstudio/src/pages/App.jsx
+++ szahstudio/src/pages/App.jsx
@@ -1,11 +1,22 @@
-import '../styles/App.css';
+import '../styles/index.css';
+import About from '../components/About';
+import Banner from '../components/Banner';
+import Contact from '../components/Contact';
+import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import Projects from '../components/Projects';
+import Services from '../components/Services';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <About/>
+      <Banner/>
+      <Contact/>
+      <Header/>
       <Navbar/>
+      <Projects/>
+      <Services/>
     </div>
   );
 }
