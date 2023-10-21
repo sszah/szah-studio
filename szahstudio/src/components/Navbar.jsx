84eff739d476bef068f42a40bec812fa85fca0a2
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-10-21 10:58:04 +0700

    tailwind

---
+++ szahstudio/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class Navbar extends PureComponent {
+  render() {
+    return (
+      <div>Ini navbar</div>
+    )
+  }
+}
+
+export default Navbar
