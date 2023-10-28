bab01c9b35e7fe4a9fe8cc9677c8d26b9fb2ab10
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-10-28 17:39:05 +0700

    project & contact

--- szahstudio/src/components/Contact.jsx
+++ szahstudio/src/components/Contact.jsx
@@ -4,7 +4,40 @@ import { fadeIn } from '../variant';
 
 const Contact = () => {
   return (
-    <div className='section' id='contact'>Contact</div>
+    <section className='py-16 lg:section' id='contact'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          {/* text */}
+          <div className='flex-1'>
+            <div>
+              <h4 className='text-xl uppercase text-accent font-medium mb-2 tracking-wide'>Get In Touch</h4> 
+              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Let's Work <br /> Together!</h2>
+            </div>
+          </div>
+
+          {/* form */}
+        <form className='flex-1 border rounded-2x1 flex flex-col gap-y-6 pb-12 p-6 items-start'>
+          <input 
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
+            type="text" 
+            placeholder='Your Name'
+          />
+
+          <input 
+            className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all'
+            type="text" 
+            placeholder='Your Email'
+          />
+
+          <textarea 
+            className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12'
+            placeholder='Your Message'
+          ></textarea>
+          <button className='btn btn-lg'>Send Message</button>
+        </form>
+      </div>
+      </div>
+    </section>
   )
 }
 
