9c55ff007e0ab03434c87505f9296fcadddb14e5
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-10-30 03:59:56 +0700

    input project

--- szahstudio/src/pages/App.jsx
+++ szahstudio/src/pages/App.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
 import Services from '../components/Services';
+import InputProject from '../components/Inputproject';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
       <Services/>
       <Projects/>
       <Contact/>
+      <InputProject/>
       <Navbar/>
+
     </div>
   );
 }
